c53629286bde9b12139b1d60126756a04661bab6
RaydenDarkus <55951556+RaydenDarkus@users.noreply.github.com>
2019-09-29 20:43:23 +0530

    Add files via upload

---
+++ Exp12_Rectangle.java
@@ -0,0 +1,12 @@
+public class Exp12_Rectangle extends Exp12_Base
+{
+    public void read(float x,float y)
+    {
+        l=x;
+        b=y;
+    }
+    public void calculate()
+    {
+        area=l*b;
+    }
+}
