9bc048dbbaa3b8cc5e5cb1ef94c311103494e875
RaydenDarkus <55951556+RaydenDarkus@users.noreply.github.com>
2019-09-29 20:37:31 +0530

    Add files via upload

---
+++ Exp12_Circle12.java
@@ -0,0 +1,12 @@
+public class Exp12_Circle12 extends Exp12_Base
+{
+
+    public void read(float x)
+    {
+        r=x;
+    }
+    public void calculate()
+    {
+        area=(float)(3.14*r*r);
+    }
+}
